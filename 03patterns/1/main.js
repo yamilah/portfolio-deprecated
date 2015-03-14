95935cf62c7545732ed898fbc4771b377302b118
Yamilah Atallah <yami@yamilah.com>
2015-03-14 19:46:02 -0400

    Initial commit, first pattern.

---
+++ 03patterns/1/main.js
@@ -0,0 +1,42 @@
+Logoish.init();
+
+var colors = ["#1DB7CF", "#E03D24"];
+
+speed(0);
+lineColor("black");
+lineWidth(15);
+
+function arch() {
+angle(0);
+forward(50);
+rotate(60);
+forward(25);
+rotate(300);
+forward(50);
+
+rotate(60);
+forward(25);
+rotate(300);
+forward(50);
+
+rotate(180);
+forward(50);
+rotate(300);
+forward(25);
+
+rotate(60);
+forward(50);
+rotate(300);
+forward(25);
+
+rotate(60);
+forward(50);
+}
+
+for(var archLoop = 0; archLoop < 10; archLoop++) {
+    moveTo(10,archLoop*74+180);
+    lineColor(colors[archLoop%2]);
+    for(var archIndex = 0; archIndex < 10; archIndex++) {
+        arch();
+    }
+}
