24c285c9cc9521454a3274040af9d5f6b6f9a992
Yamilah Atallah <yami@yamilah.com>
2015-03-14 22:58:53 -0400

    Added second pattern.

---
+++ 03patterns/2/main.js
@@ -0,0 +1,21 @@
+(function() {
+    Logoish.init();
+    speed(0);
+
+    function square() {
+        for (var i = 0; i < 4; i++) {
+            forward(150);
+            rotate(89.8);
+        }
+        rotate(15);
+    }
+
+    move(150, -150);
+
+    for (var j = 0; j < 60; j++) {
+        for (var i = 0; i < 19; i++) {
+            square();
+        }
+        forward(250);
+    }
+})();
