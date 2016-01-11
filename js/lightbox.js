65bfa614f0e49d415bd4b56ee4fa0a0c7e3e891d
Yamilah Atallah <yami@yamilah.com>
2016-01-10 22:03:13 -0500

    Implemented modal.

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,28 +1,13 @@
 $(document).ready(function() {
-    $("#iconWrapper").click(function() {
-        $("aside nav").slideToggle();
-    });
-
-    $("nav li").click(function() {
-        $(this).toggleClass("selected");
-        var dataType = $(this).data("type");
-
-        $("[data-"+dataType+"]").slideToggle();
-    });
-
-    $(".imageWrapper figure").click(function() {
-        $("#lightbox-" + this.id).show();
-    });
-
-    $(".imageWrapper p").click(function() {
-        $("#lightbox-" + this.parentElement.previousElementSibling.id).show();
-    });
-
-    $(".closeImage").click(function() {
-        $(this.parentElement.parentElement.parentElement.parentElement).hide();
-    });
-
-    $(".blackOverlay").click(function() {
-        $(this.parentElement).hide();
-    });
-});
+  $(".fade-wrapper").click(function() {
+    $("#lightbox-" + this.id).show();
+  })
+  
+  $(".black-overlay").click(function() {
+    $(this.parentElement).hide();
+  })
+  
+  $(".lightbox-position-wrapper").click(function() {
+    $(this).hide();
+  })
+});    
